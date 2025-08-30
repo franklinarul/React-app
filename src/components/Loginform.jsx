194472536433633dd145fabd307f92b9c201cae2
franklinarul <franklinarul24@gmail.com>
2025-08-30 13:22:06 +0530

    responsive update

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -30,21 +30,21 @@ function Loginfrom(){
 
     return(
         <div className="flex justify-center h-screen items-center">
-        <div className="grid grid-cols-2 w-2/3 h-2/3 border-2 border-solid border-black">
+        <div className="grid grid-cols-2 lg:w-2/3 mx-6 h-2/3 border-2 border-solid border-black">
             <div className="text-center flex flex-col justify-center">
                 <h1 className="text-4xl pb-7">sign in</h1>
                 <p className="pb-3">or use your account</p>
-                <input onChange={handlename} className="bg-gray-200 p-2 lg:mx-20 sm:mx-3 mb-3 text-black" type="email" placeholder="enter your email" />
+                <input onChange={handlename} className="bg-gray-200 p-2 lg:mx-20 mx-3 mb-3 text-black" type="email" placeholder="enter your email" />
                 <br />
-                <input onChange={handlepass} className="bg-gray-200 p-2 lg:mx-20 sm:mx-3" type="number" placeholder="password" name="" id="" />
+                <input onChange={handlepass} className="bg-gray-200 p-2 lg:mx-20 mx-3" type="number" placeholder="password" name="" id="" />
                 <p className="pt-3 pb-3">forgot your password?</p>
-                <button onClick={check} className="bg-red-500 rounded-full lg:mx-20 sm:mx-3" >sign in</button>
+                <button onClick={check} className="bg-red-500 rounded-full lg:mx-20 mx-3" >sign in</button>
             </div>
             <div className="bg-red-500 text-center flex flex-col justify-center">
-                <h1 className="lg:text-4xl sm:text-3xl pb-10 ">Hello,friend!</h1>
+                <h1 className="lg:text-4xl text-2xl pb-10 ">Hello,friend!</h1>
                 <p className="text-white text-xl">enter your Login details and start</p><br />
                 <p className="text-white text-xl">journey with us</p>
-                <button onClick={check} className="lg:mx-20 sm:mx-3 mt-5">sign up</button>
+                <button onClick={check} className="lg:mx-20 mx-3 mt-5">sign up</button>
             </div>
             </div>
             </div>
